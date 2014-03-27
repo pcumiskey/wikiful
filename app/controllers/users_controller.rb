c6bb5331404955e4fc880a601d687e6d07f616f9
Patrick Cumiskey <pcumiskey@gmail.com>
2014-03-27 15:03:13 +0000

    added security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@
   def create
    @user = User.new(user_params)
    if @user.save
+       session[:user_id] =@user.id
        redirect_to root_url, notice: "Thank you for Signing Up"
     else
       render "new"   
